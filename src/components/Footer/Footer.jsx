d2fa40d93d79e18433310d1b949af322a40c7519
Flocky05 <md.rakibul.islam.0501@gmail.com>
2023-09-15 21:38:43 +0600

    added footer section

---
+++ src/components/Footer/Footer.jsx
@@ -0,0 +1,15 @@
+
+
+const Footer = () => {
+    return (
+        <div className="bg-[#031B34]">
+            <div className="p-20 text-center">
+                <h2 className="text-4xl text-center font-semibold">Do you want to step in to the </h2>
+                <h2 className="text-4xl text-center ">future before others</h2>
+                <button className="px-8 py-3 border text-white m-20 hover:bg-orange-600">Request Early access</button>
+            </div>
+        </div>
+    );
+};
+
+export default Footer;
