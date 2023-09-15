36ce79db73c9cbf0007b451822662a5dba954ed4
Flocky05 <md.rakibul.islam.0501@gmail.com>
2023-09-15 20:53:05 +0600

    added offer section

--- src/components/CaseStudies/CaseStudies.jsx
+++ src/components/CaseStudies/CaseStudies.jsx
@@ -1,18 +1,22 @@
 
 import img from "../../assets/possibility.png"
+import Offer from "../Offer/Offer";
 const CaseStudies = () => {
     return (
-        <div className="flex justify-between items-center m-20 min-h-screen">
-           <img className="w-2/4" src={img} alt="" />
-           <div className="p-4">
-                <p className="text-blue-500 py-8">Request Early Access to Get Started</p>
-                <div className="py-8">
-                    <h2>The possibilities are </h2>
-                    <h2>beyond your imagination </h2>
-                </div>
-                <p className="py-8">Yet bed any for travelling assistance indulgence unpleasing. Not thoughts all exercise blessing. Indulgence way everything joy alteration boisterous the attachment. Party we years to order allow asked of.</p>
-                <p className="text-orange-600 py-8">Request Early Access to Get Started</p>
-           </div>
+        <div>
+            <div className="flex justify-between items-center m-20 min-h-screen">
+            <img className="w-2/4" src={img} alt="" />
+            <div className="p-4">
+                    <p className="text-blue-500 py-8">Request Early Access to Get Started</p>
+                    <div className="py-8">
+                        <h2>The possibilities are </h2>
+                        <h2>beyond your imagination </h2>
+                    </div>
+                    <p className="py-8">Yet bed any for travelling assistance indulgence unpleasing. Not thoughts all exercise blessing. Indulgence way everything joy alteration boisterous the attachment. Party we years to order allow asked of.</p>
+                    <p className="text-orange-600 py-8">Request Early Access to Get Started</p>
+            </div>
+            </div>
+            <Offer></Offer>
         </div>
     );
 };
---
+++ src/components/Offer/Offer.jsx
@@ -0,0 +1,15 @@
+
+
+const Offer = () => {
+    return (
+        <div className="flex justify-between items-center bg-gradient-to-r from-fuchsia-700 to-orange-500 p-20 m-20 rounded-lg">
+            <div>
+                <p className="text-sm">Request Early Access to Get Started</p>
+                <h3 className="text-3xl py-10 font-semibold">Register today & start exploring the endless possiblities.</h3>
+            </div>
+            <button className="bg-black px-8 py-2 rounded-lg"> Get Start</button>
+        </div>
+    );
+};
+
+export default Offer;
