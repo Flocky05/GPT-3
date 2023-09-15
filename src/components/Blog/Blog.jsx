764ccb838db1bf7f37bd0b84a5de929a7a657616
Flocky05 <md.rakibul.islam.0501@gmail.com>
2023-09-15 18:16:08 +0600

    update blog section

--- src/components/Blog/Blog.jsx
+++ src/components/Blog/Blog.jsx
@@ -2,8 +2,32 @@
 
 const Blog = () => {
     return (
-        <div>
-            <h3>This is blog section bro</h3>
+        <div className="bg-blue-900 m-10 p-20 min-h-screen">
+            <div className=" flex  justify-center items-center">
+                <h3 className="text-2xl font-semibold w-2/4">What is GPT-3</h3>
+                <p>We so opinion friends me message as delight. Whole front do of plate heard oh ought. His defective nor convinced residence own. Connection has put impossible own apartments boisterous. At jointure ladyship an insisted so humanity he. Friendly bachelor entrance to on by.</p>
+            </div>
+            <div className="flex justify-between py-10">
+                <div>
+                    <h2>The possibilities are beyond </h2>
+                    <h2>your imagination</h2>
+                </div>
+                <p className="text-orange-500">Explore The Library</p>
+            </div>
+            <div className="grid grid-cols-3 p-10">
+                <div className="p-5">
+                    <h3 className="text-2xl font-bold py-5">Chatbots</h3>
+                    <p className="py-10 text-lg">We so opinion friends me message as delight. Whole front do of plate heard oh ought. </p>
+                </div>
+                <div className="p-5">
+                    <h3 className="text-2xl font-bold py-5">Knowledgebase</h3>
+                    <p className="py-10 text-lg">We so opinion friends me message as delight. Whole front do of plate heard oh ought. </p>
+                </div>
+                <div className="p-5">
+                    <h3 className="text-2xl font-bold py-5">Education</h3>
+                    <p className="py-10 text-lg">We so opinion friends me message as delight. Whole front do of plate heard oh ought. </p>
+                </div>
+            </div>
         </div>
     );
 };
