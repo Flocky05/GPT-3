34d1501287710cd5ebb571ef00aa2adad4b8bbd6
Flocky05 <md.rakibul.islam.0501@gmail.com>
2023-09-17 13:01:10 +0600

    resposive home page

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -7,19 +7,19 @@ import "./Home.css"
 const Home = () => {
     return (
         <div className="m-20">
-                    <div className="grid grid-cols-2 items-center">
-                <div className="p-8">
-                    <div className="">
+                    <div className="grid grid-cols-1 lg:grid-cols-2 items-center">
+                <div className="lg:p-8">
+                    <div>
                         <h2>Let’s Build Something</h2>
                         <h2>amazing with GPT-3</h2>
                         <h2>OpenAI</h2>
                     </div>
                     <p className="text-lg">Yet bed any for travelling assistance indulgence unpleasing. Not thoughts all exercise blessing. Indulgence way everything joy alteration boisterous the attachment. Party we years to order allow asked of.</p>
                     <div className=" relative py-4">
-                        <input className="bg-blue-950  rounded-md w-3/5 py-3 p-2" type="text" placeholder="Your Email Address" />
-                        <button className="bg-orange-500 hover:bg-orange-700 py-3 px-10 absolut -ml-36 rounded-lg"> Get Start</button>
+                        <input className="bg-blue-950  rounded-md  lg:w-3/5 py-3 p-2" type="text" placeholder="Your Email Address" />
+                        <button className="bg-orange-500 hover:bg-orange-700 py-3 px-6 lg:px-10 absolut -mr-40 lg:-ml-36 rounded-lg"> Get Start</button>
                     </div>
-                    <div className="flex items-center py-5">
+                    <div className="lg:flex items-center py-5">
                         <img src={img2} alt="" />
                         <p className="p-3 text-sm">1,600 people requested access a visit in last 24 hours</p>
                     </div>
