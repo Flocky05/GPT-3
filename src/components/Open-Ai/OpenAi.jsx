2cd5f61fba70baf20421a6d82e2aa78d3c405f87
Flocky05 <md.rakibul.islam.0501@gmail.com>
2023-09-17 16:10:12 +0600

    update openAI section

--- src/components/Open-Ai/OpenAi.jsx
+++ src/components/Open-Ai/OpenAi.jsx
@@ -2,35 +2,47 @@
 
 const OpenAi = () => {
     return (
-        <div className="flex justify-between p-20">
+        <div className="lg:flex justify-between p-10 lg:p-20">
            <div>
                 <h2 className="text-3xl font-bold w-full p-10">The Future is Now and You Just Need To Realize It. Step into Future Today & Make it Happen.</h2>
                 <p className="text-orange-500 p-10">Request Early Access to Get Started</p>
            </div>
-          <div className="grid grid-cols-2">
+          <div>
                 <div>
-                    <div className="p-14">
-                        <h3>Improving end  </h3>
-                        <h3>distrusts instantly</h3>
+                    <div className="grid grid-cols-2">
+                        <div className="p-14">
+                            <h3>Improving end  </h3>
+                            <h3>distrusts instantly</h3>
+                        </div>
+                        <p className="py-14 text-blue-400">From they fine john he give of rich he. They age and draw mrs like. Improving end distrusts may instantly was household applauded.</p>
                     </div>
-                    <div className="p-14">
-                        <h3>Become the </h3>
-                        <h3>tended active</h3>
+                    <div className="grid grid-cols-2">
+                        <div className="p-14">
+                            <h3>Become the </h3>
+                            <h3>tended active</h3>
+                        </div>
+                        <p className="py-14 text-blue-400">Considered sympathize ten uncommonly occasional assistance sufficient not. Letter of on become he tended active enable to.</p>
                     </div>
-                    <div className="p-14">
-                        <h3>Message or  </h3>
-                        <h3>am nothing</h3>
+                    <div className="grid grid-cols-2">
+                        <div className="p-14">
+                            <h3>Message or  </h3>
+                            <h3>am nothing</h3>
+                        </div>
+                        <p className="py-14 text-blue-400">Led ask possible mistress relation elegance eat likewise debating. By message or am nothing amongst chiefly address.</p>
                     </div>
-                    <div className="p-14">
-                        <h3>Really boy  </h3>
-                        <h3>law county</h3>
+                    <div className="grid grid-cols-2">
+                        <div className="p-14">
+                            <h3>Really boy  </h3>
+                            <h3>law county</h3>
+                        </div>
+                        <p className="py-14 text-blue-400">Really boy law county she unable her sister. Feet you off its like like six. Among sex are leave law built now. In built table in an rapid blush.</p>
                     </div>
                 </div>
-                <div className="text-blue-400">
-                        <p className="py-12">From they fine john he give of rich he. They age and draw mrs like. Improving end distrusts may instantly was household applauded.</p>
-                        <p className="py-12">Considered sympathize ten uncommonly occasional assistance sufficient not. Letter of on become he tended active enable to.</p>
-                        <p className="py-12">Led ask possible mistress relation elegance eat likewise debating. By message or am nothing amongst chiefly address.</p>
-                        <p className="py-12">Really boy law county she unable her sister. Feet you off its like like six. Among sex are leave law built now. In built table in an rapid blush.</p>
+                <div className="">
+                        
+                        
+                        
+                        
                 </div>
           </div>
         </div>
