64c92cbf49d35682bbc06ab43179a11ebf21cc34
Flocky05 <md.rakibul.islam.0501@gmail.com>
2023-09-17 16:00:41 +0600

    responsive library section

--- src/components/Library/Library.jsx
+++ src/components/Library/Library.jsx
@@ -6,21 +6,21 @@ import img5 from "../../assets/blog05.png"
 
 const Library = () => {
     return (
-        <div className="p-20">
+        <div className="p-8 lg:p-20">
            <div className="text-2xl font-bold">
                 <h2 className="p-5">A lot is happening, </h2>
                 <h2 className="p-5">We are blogging about it.</h2>
            </div>
-           <div className="flex justify-between">
+           <div className="lg:flex justify-between">
                 <div className="p-8 w-full">
                     <img className="w-full" src={img1} alt="" />
                     <div className="bg-[#042C54] p-4">
                         <p className="text-sm">Sep 26, 2021</p>
-                        <p className="py-28 text-lg ">GPT-3 and Open  AI is the future. Let us exlore how it is?ChatGPT, which stands for Chat Generative Pre-trained Transformer, is a large language model–based chatbot developed by OpenAI and launched on November 30, 2022, which enables users to refine and steer a conversation towards a desired length, format, style, level of detail, and language used.</p>
+                        <p className="py-8 lg:py-28 text-sm lg:text-lg ">GPT-3 and Open  AI is the future. Let us exlore how it is?ChatGPT, which stands for Chat Generative Pre-trained Transformer, is a large language model–based chatbot developed by OpenAI and launched on November 30, 2022, which enables users to refine and steer a conversation towards a desired length, format, style, level of detail, and language used.</p>
                         <p className="py-4">lorem</p>
                     </div>
                 </div>
-            <div className="grid grid-cols-2">
+            <div className="grid grid-cols-1 lg:grid-cols-2">
                 <div className="p-8">
                     <img className="w-full" src={img2} alt="" />
                     <div className="bg-[#042C54] p-4">
